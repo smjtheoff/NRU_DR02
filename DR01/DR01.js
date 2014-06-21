8a57e89fe07f9a8db04bb3510852db727aaa4013
smjtheoff <smjtheoff@Pongsakorns-MacBook-Pro.local>
2014-06-22 02:54:03 +0700

    Add Base Dynamic Layers

--- DR01/DR01.js
+++ DR01/DR01.js
@@ -3,14 +3,16 @@ require([
 	"esri/map", "esri/layers/ArcGISDynamicMapServiceLayer", 
 	"esri/TimeExtent", "esri/dijit/TimeSlider",
 	"dojo/_base/array", "dojo/dom", "dojo/domReady!"
-], function(
-	Map, ArcGISDynamicMapServiceLayer, 
-	TimeExtent, TimeSlider,
-	arrayUtils, dom
-){
+], function(Map, ArcGISDynamicMapServiceLayer, TimeExtent, TimeSlider, arrayUtils, dom){
 	map = new Map("mapDiv" ,
 	{
-		basemap : [streets],
-		center : []
-	})
-}
+		basemap : "hybrid",
+		center : [102.697870,16.014687],
+		zoom : 7
+	});
+
+	var ndviLayer = new ArcGISDynamicMapServiceLayer("http://ags.giskku.in.th/ArcGISServer/rest/services/NRU/NRU-MODIS/MapServer");
+	//ndviLayer.setVisibleLayers([0]);
+	map.addLayers([ndviLayer]);
+
+});
