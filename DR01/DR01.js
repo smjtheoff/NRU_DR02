b4ae474df3a277b68e70501b441dd9a6a5500387
Pongsakorn Udombua <smjtheoff@infosci.in.th>
2014-06-22 00:47:17 +0700

    add init function

--- DR01/DR01.js
+++ DR01/DR01.js
@@ -1,2 +1,16 @@
 var map;
-require ();
+require([
+	"esri/map", "esri/layers/ArcGISDynamicMapServiceLayer", 
+	"esri/TimeExtent", "esri/dijit/TimeSlider",
+	"dojo/_base/array", "dojo/dom", "dojo/domReady!"
+], function(
+	Map, ArcGISDynamicMapServiceLayer, 
+	TimeExtent, TimeSlider,
+	arrayUtils, dom
+){
+	map = new Map("mapDiv" ,
+	{
+		basemap : [streets],
+		center : []
+	})
+}
